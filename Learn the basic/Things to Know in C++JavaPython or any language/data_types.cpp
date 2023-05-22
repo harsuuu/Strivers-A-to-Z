e228cad5acac8266149d9075020f456e2d2114b8
harsuuu <harshjoshi7895@gmail.com>
2023-05-22 20:51:52 +0530

    Size of data types

---
+++ Learn the basic/Things to Know in C++JavaPython or any language/data_types.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int dataTypeSize(string str) {
+        if(str == "Character")
+            return sizeof(char);
+        else if(str == "Integer")
+            return sizeof(int);
+        else if(str == "Long")
+            return sizeof(long);
+        else if(str == "Float")
+            return sizeof(float);
+        else
+            return sizeof(double);
+
+    }
+};
